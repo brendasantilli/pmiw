e929cd75517832ee1ad0e8f5ed4aa7ef8c91cbda
Brendis <66231353+brendasantilli@users.noreply.github.com>
2024-10-16 22:57:17 -0300

    Add files via upload

---
+++ tpfinalparte1.js
@@ -0,0 +1,84 @@
+let pantallaActual = 1; 
+let imagenes = [];
+let textos = [];
+let desiciones = {
+  4: [5, 8],
+  9: [10, 11],
+  12: [13, 15]
+};
+
+function preload() {
+  for (let i = 1; i <= 17; i++) {
+    imagenes[i] = loadImage("data/pantalla" + i + ".jpeg");
+  }
+  textos = loadStrings('data/textos.txt');
+}
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(220);
+  DibujarTextoConFondo();
+  dibujarBotones();
+}
+
+function DibujarTextoConFondo() {
+  image(imagenes[pantallaActual], 0, 0, width, height);
+
+  let padding = 30;
+  let texto = textos[pantallaActual];
+  let anchoTextoMaximo = 580;
+  let rectX = width / 2; 
+  let rectY = height - 200;
+
+  fill(0, 0, 0, 150);
+  noStroke();
+  rectMode(CENTER, CENTER);
+  rect(rectX, rectY, anchoTextoMaximo + padding, 80 + padding); 
+  
+  fill(255);
+  textSize(20);
+  textAlign(CENTER, TOP); 
+  textWrap(WORD);
+  text(texto, rectX, rectY - padding, anchoTextoMaximo); 
+}
+
+function dibujarBotones() {
+  fill(0, 255, 0); 
+  rect(width - 110, height - 50, 100, 40); 
+  fill(255);
+  textSize(16);
+  textAlign(CENTER, CENTER);
+  text("Siguiente", width-110 , height-30); 
+
+  if (pantallaActual > 1) {
+    fill(255, 0, 0); 
+    rect(10, height - 50, 100, 40); 
+    fill(255);
+    text("Anterior", 60, height - 30); 
+  }
+}
+
+function mousePressed() {
+  if (mouseY >= height - 50 && mouseY <= height - 10) {
+    if (mouseX >= width - 110 && mouseX <= width - 10) {
+      irPantallaSiguiente();
+    } else if (pantallaActual > 1 && mouseX >= 10 && mouseX <= 110) {
+      irPantallaAnterior();
+    }
+  }
+}
+
+function irPantallaAnterior() {
+  if (pantallaActual > 1) {
+    pantallaActual--;
+  }
+}
+
+function irPantallaSiguiente() {
+  if (pantallaActual < 17) {
+    pantallaActual++;
+  }
+}
