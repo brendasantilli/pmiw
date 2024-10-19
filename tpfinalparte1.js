13ac6c526e65cd4a4de3c9dcecf0b00381304338
Brendis <66231353+brendasantilli@users.noreply.github.com>
2024-10-19 11:16:46 -0300

    Add files via upload

--- tpfinalparte1.js
+++ tpfinalparte1.js
@@ -1,6 +1,7 @@
 let pantallaActual = 0;
 let imagenes = [];
 let textos = [];
+let sonidos = {};
 let Pantallas = ["inicio", 1, 2, 3, 4,
   5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
   16, 17, "creditos"];
@@ -18,10 +19,16 @@ function preload() {
     imagenes[i] = loadImage("data/pantalla" + i + ".jpeg");
   }
   textos = loadStrings('data/textos.txt');
+    soundFormats('mp3');
+  sonidos['inicio'] = loadSound('data/inicio.mp3');
+  sonidos['boton'] = loadSound('data/boton.mp3');
+  sonidos['creditos'] = loadSound('data/creditos.mp3');
 }
 
 function setup() {
   createCanvas(640, 480);
+  anchoBotones = 100;
+  altoBotones = 40;
 }
 
 function draw() {
@@ -36,37 +43,38 @@ function draw() {
     botonReiniciar()
   } else if (Pantallas[pantallaActual] === "creditos") {
     PantallaCreditos();
-    botonReiniciar()
+    botonReiniciar();
   } else {
     dibujarPantallas();
     botonSiguiente();
   }
 }
-
 function mousePressed() {
   if (Pantallas[pantallaActual] === "inicio") {
-    if (mouseX >= width / 2 - 75 && mouseX <= width / 2 + 75 && mouseY >= height / 2 - 25 && mouseY <= height / 2 + 25) {
+    if (mouseDentroDeRect(width / 2 - 75, height / 2 - 25, 150, 50)) {
       pantallaActual = 1;
     }
   } else if (Pantallas[pantallaActual] === "creditos" || pantallaActual === 7 || pantallaActual === 10 || pantallaActual === 14) {
-    if (mouseX >= width / 2 - 100 && mouseX <= width / 2 + 100 && mouseY >= height / 2 + 120 && mouseY <= height / 2 + 180) {
+    if (mouseDentroDeRect(width / 2 - 100, height / 2 + 120, 200, 60)) {
       pantallaActual = 0;
     }
   } else {
-    if (mouseY >= botonesY - (altoBotones / 2) && mouseY <= botonesY + (altoBotones / 2)) {
-      if (mouseX >= botonSigX - (anchoBotones / 2) && mouseX <= botonSigX + (anchoBotones / 2)) {
-        irPantallaSiguiente();
-      }
+    if (mouseDentroDeRect(botonSigX - (anchoBotones / 2), botonesY - (altoBotones / 2), anchoBotones, altoBotones)) {
+      irPantallaSiguiente();
     }
 
     let decisionY = height - 100;
     let botonA_X = width / 2 - 70;
     let botonB_X = width / 2 + 70;
 
-    if (mouseX >= botonA_X - 60 && mouseX <= botonA_X + 60 && mouseY >= decisionY - 20 && mouseY <= decisionY + 20) {
+    if (mouseDentroDeRect(botonA_X - 60, decisionY - 20, 120, 40)) {
       pantallaActual = decisiones[Pantallas[pantallaActual]][0];
-    } else if (mouseX >= botonB_X - 60 && mouseX <= botonB_X + 60 && mouseY >= decisionY - 20 && mouseY <= decisionY + 20) {
+    } else if (mouseDentroDeRect(botonB_X - 60, decisionY - 20, 120, 40)) {
       pantallaActual = decisiones[Pantallas[pantallaActual]][1];
     }
   }
+}
+
+function mouseDentroDeRect(x, y, ancho, alto) {
+  return mouseX >= x && mouseX <= x + ancho && mouseY >= y && mouseY <= y + alto;
 }
